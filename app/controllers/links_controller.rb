a550f48cf0d6c24a1664c7fe560c830f41eeb7c6
Tom Scott <tubbo@psychedeli.ca>
2013-08-03 22:03:34 -0400

    Build Links API

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,53 @@
 class LinksController < ApplicationController
+  respond_to :json
+
+  def index
+    @links = resource.where search_params
+
+    respond_with @links
+  end
+
+  def show
+    @link = resource.find params[:id]
+
+    respond_with @link
+  end
+
+  def create
+    @link = resource.new post_params
+
+    if @link.save
+      respond_with @link, notice: 'New link posted.'
+    else
+      render json: { errors: @link.errors.full_messages }
+    end
+  end
+
+  def destroy
+    @link = resource.find params[:id]
+    with_message = if @link.destroy
+      { notice: "Link removed." }
+    else
+      { alert: "Link could not be removed." }
+    end
+
+    redirect_to links_path, with_message
+  end
+
+  private
+  def resource
+    if @tag.present?
+      @tag.links
+    else
+      Link
+    end
+  end
+
+  def search_params
+    params.permit(:title, :tag)
+  end
+
+  def post_params
+    params.require(:link).permit(:title, :url, :tag_id, :service_id)
+  end
 end
--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,5 +1,106 @@
 require 'spec_helper'
 
 describe LinksController do
+  let(:link) { links :afterhours }
 
+  describe "viewing a collection" do
+    context "of all links" do
+      before { get :index, format: 'json' }
+
+      it "responds successfully" do
+        response.should be_success
+      end
+
+      it "includes a collection of links" do
+        assigns(:links).should_not be_empty
+      end
+
+      it "renders those links to json" do
+        json_response = JSON.parse(response.body)
+
+        json_response.should_not be_nil
+        json_response['links'].should_not be_empty
+      end
+    end
+
+    context "of links that match search filters" do
+      before { get :index, title: link.title, format: 'json' }
+
+      it "responds successfully" do
+        response.should be_success
+      end
+
+      it "includes the link that was searched for" do
+        assigns(:links).should_not be_empty
+        assigns(:links).should include(link)
+      end
+    end
+  end
+
+  describe "viewing a single link" do
+    context "that has been posted to the site" do
+      before { get :show, id: link.id, format: 'json' }
+
+      it "responds successfully" do
+        response.should be_success
+      end
+
+      it "renders the link in json" do
+        json_response = JSON.parse response.body
+
+        json_response.should_not be_nil
+        json_response['link'].should_not be_empty
+      end
+    end
+
+    context "that does not exist" do
+      before { get :show, id: link.id+1, format: 'json' }
+
+      it "responds with a 404 error" do
+        response.should be_missing
+      end
+    end
+  end
+
+  describe "posting a new link" do
+    context "with valid parameters" do
+      before { post :create, link: { title: 'test', url: 'http://example.com' } }
+
+      it "redirects to the post" do
+        response.should be_success
+        JSON.parse(response.body).should include('link')
+      end
+    end
+
+    context "with invalid parameters" do
+      before { post :create, link: { title: 'hello' } }
+
+      it "responds with validation messages" do
+        JSON.parse(response.body).should include('errors')
+      end
+    end
+  end
+
+  describe "removing a link" do
+    context "that has already been posted" do
+      let(:link_id) { link.id }
+      before { delete :destroy, id: link_id }
+
+      it "removes the link from the db" do
+        Link.find(link_id).should be_nil
+      end
+
+      it "redirects to index" do
+        response.should be_redirect
+      end
+    end
+
+    context "that has not been posted" do
+      before { delete :destroy, id: link_id+1 }
+
+      it "redirects to index with an alert" do
+        response.should be_redirect
+      end
+    end
+  end
 end
