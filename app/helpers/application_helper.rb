f0961e2132525612486e179cded82b187e918312
Tom Scott <tubbo@psychedeli.ca>
2013-08-03 22:54:06 -0400

    Refactor application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def title_tag
+    content_tag :title, title_content
+  end
+
+  def title_content
+    if content_for? :title
+      content_for :title
+    else
+      "choongang: the top 40 of the internet"
+    end
+  end
 end
