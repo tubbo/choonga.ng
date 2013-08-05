066e3b87d79c3922f8b85934e23dafe4559a7104
Tom Scott <tubbo@psychedeli.ca>
2013-08-04 23:00:06 -0400

    Add admin user in seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+unless AdminUser.where(name: %w(admin@example.com admin@choonga.ng)).any?
+  AdminUser.new(
+    name: 'admin@choonga.ng',
+    password: 'choongang',
+    password_confirmation: 'choongang'
+  ).save(validate: false)
+end
